f279f0e358410615c35fa8eba6ed3a45e011f3b5
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-04-04 13:16:47 -0700

    optimize erc20 faucet

--- contracts/ERC20Faucet.sol
+++ contracts/ERC20Faucet.sol
@@ -8,113 +8,56 @@ import 'hardhat/console.sol';
 
 contract ERC20Faucet is Ownable {
     ERC20 private erc20Token;
-    uint256 private erc20DripAmount;
+    uint256 private dripAmount;
     uint256 private timeout;
     bool private faucetDisabled;
     bool private singleUse;
 
     mapping(address => bool) private hasUsedFaucet;
     mapping(address => uint256) private timeouts;
-    mapping(address => bool) private blockedAccounts;
-
-    event SentERC20Tokens(address indexed account, uint256 timestamp);
 
     constructor(
         ERC20 _erc20Token,
-        uint256 _erc20DripAmount,
+        uint256 _dripAmount,
         uint256 _timeout,
         bool _singleUse
     ) {
         erc20Token = _erc20Token;
-        erc20DripAmount = _erc20DripAmount;
+        dripAmount = _dripAmount;
         timeout = _timeout;
         singleUse = _singleUse;
     }
 
     function dripERC20(address _to) external {
-        _canUseFaucet(_to);
-        bool success = erc20Token.transfer(_to, erc20DripAmount);
-        require(success, 'send fail');
-        setAccountTimeout(_to, block.timestamp + timeout);
-        setHasUsedFaucet(_to);
-        emit SentERC20Tokens(_to, block.timestamp);
-    }
-
-    function _canUseFaucet(address _to) private view {
-        _isfaucetDisabled();
-        _isBlockedAccount(_to);
-        _isTimedOut(_to);
-        _checkSingleUse(_to);
-    }
-
-    function _isfaucetDisabled() private view {
         require(!faucetDisabled, 'drip disabled');
-    }
 
-    function _isBlockedAccount(address _account) private view {
-        require(!getBlockedAccount(_account), 'address blocked');
-    }
-
-    function _isTimedOut(address _to) private view {
-        require(getAccountTimeout(_to) <= block.timestamp, 'too early');
-    }
-
-    function _checkSingleUse(address _to) private view {
         if (singleUse) {
-            require(!getHasUsedFaucet(_to), 'already used');
+            require(!hasUsedFaucet[_to], 'already used');
         }
+
+        require(timeouts[_to] <= block.timestamp, 'too early');
+
+        bool success = erc20Token.transfer(_to, dripAmount);
+        
+        require(success, 'send fail');
+
+        timeouts[_to] = block.timestamp + timeout;
+        hasUsedFaucet[_to] = true;
     }
 
-    function getFaucetDisabled() public view onlyOwner returns (bool) {
-        return faucetDisabled;
-    }
-
-    function setFaucetDisabled(bool _val) public onlyOwner {
+    function setDisableFaucet(bool _val) external onlyOwner {
         faucetDisabled = _val;
     }
 
-    function getBlockedAccount(address _account) public view returns (bool) {
-        return blockedAccounts[_account];
-    }
-
-    function setBlockedAccount(address _account, bool _val) public onlyOwner {
-        blockedAccounts[_account] = _val;
-    }
-
-    function getSingleUse() public view returns (bool) {
-        return singleUse;
-    }
-
-    function setSingleUse(bool _shouldBeSingleUse) public onlyOwner {
+    function setSingleUse(bool _shouldBeSingleUse) external onlyOwner {
         singleUse = _shouldBeSingleUse;
     }
 
-    function getTimeout() public view returns (uint256) {
-        return timeout;
-    }
-
-    function setTimeout(uint256 _timeout) public onlyOwner {
+    function setTimeout(uint256 _timeout) external onlyOwner {
         timeout = _timeout;
     }
 
-    function getAccountTimeout(address _account)
-        public
-        view
-        onlyOwner
-        returns (uint256)
-    {
-        return timeouts[_account];
-    }
-
-    function setAccountTimeout(address _account, uint256 _seconds) internal {
-        timeouts[_account] = block.timestamp + _seconds;
-    }
-
-    function getHasUsedFaucet(address _account) public view returns (bool) {
-        return hasUsedFaucet[_account];
-    }
-
-    function setHasUsedFaucet(address _account) public onlyOwner {
-        hasUsedFaucet[_account] = true;
+    function setDripAmount(uint16 _amount) external onlyOwner {
+        dripAmount = _amount;
     }
 }
--- test/fweb3_erc20_faucet.ts
+++ test/fweb3_erc20_faucet.ts
@@ -1,25 +1,23 @@
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
 import { expect } from 'chai'
-import { BigNumber, ContractFactory } from 'ethers'
 import { ethers } from 'hardhat'
 import type { ERC20Faucet, Fweb3Token } from '../typechain-types'
 
 let fweb3Faucet: ERC20Faucet,
   fweb3Token: Fweb3Token,
   user1: SignerWithAddress,
-  user2: SignerWithAddress,
   owner: SignerWithAddress
 
 describe('erc20 faucet', () => {
   beforeEach(async () => {
-    ;[owner, user1, user2] = await ethers.getSigners()
+    ;[owner, user1] = await ethers.getSigners()
   
     const TokenFactory = await ethers.getContractFactory('Fweb3Token')
     fweb3Token = await TokenFactory.deploy()
     await fweb3Token.deployed()
   
     const FaucetFactory = await ethers.getContractFactory('ERC20Faucet')
-    fweb3Faucet = await FaucetFactory.deploy(fweb3Token.address, 1, 1, false)
+    fweb3Faucet = await FaucetFactory.deploy(fweb3Token.address, 1, 30, false)
     await fweb3Faucet.deployed()
 
     await fweb3Token.transfer(fweb3Faucet.address, ethers.utils.parseEther('666'))
@@ -35,32 +33,14 @@ describe('erc20 faucet', () => {
   it('wont allow drip if faucet disabled', async () => {
     let error: any
     try {
-      await fweb3Faucet.setFaucetDisabled(true)
+      await fweb3Faucet.setDisableFaucet(true)
       await fweb3Faucet.dripERC20(user1.address)
     } catch (e) {
       error = e
     }
-    const isDisabled = await fweb3Faucet.getFaucetDisabled()
-    expect(isDisabled).ok
     expect(error?.message.includes('drip disabled')).ok
   })
 
-  // TODO: figure out timeout
-  it.skip('only drips once for account if single use is set', async () => {
-    let error: any
-    try {
-      await fweb3Faucet.setTimeout(0)
-      await fweb3Faucet.setSingleUse(true)
-      await fweb3Faucet.dripERC20(user1.address)
-      await fweb3Faucet.dripERC20(user1.address)
-    } catch (e) {
-      error = e
-      console.log(error.message)
-    }
-    const hasUsedFaucet = await fweb3Faucet.getHasUsedFaucet(user1.address)
-    expect(hasUsedFaucet).ok
-    expect(error?.message.includes('already used')).ok
-  })
   it('wont drip for timeout', async () => {
     let error: any
     try {
@@ -70,38 +50,25 @@ describe('erc20 faucet', () => {
     } catch (e) {
       error = e
     }
-    const timeout = await fweb3Faucet.getTimeout()
-    expect(timeout).to.equal(10)
     expect(error?.message.includes('too early')).ok
   })
 
-  it('wont let a blocked account drip', async () => {
+  it('wont drip more than once if single use enabled', async () => {
     let error: any
-    try { 
-      await fweb3Faucet.setBlockedAccount(user1.address, true)
+    try {
+      await fweb3Faucet.setSingleUse(true)
+      await fweb3Faucet.dripERC20(user1.address)
       await fweb3Faucet.dripERC20(user1.address)
     } catch (e) {
       error = e
     }
-    const isBlocked = await fweb3Faucet.getBlockedAccount(user1.address)
-    expect(isBlocked).ok
-    expect(error?.message.includes('address blocked')).ok
+    expect(error?.message.includes('already used')).ok
   })
 
-  it('covers getters and setters', async () => {
-    const singleUse = await fweb3Faucet.getSingleUse()
-    expect(singleUse).not.ok
-    const newSingleUse = await fweb3Faucet.setSingleUse(true)
-    expect(newSingleUse).ok
-    const notUsedFaucet = await fweb3Faucet.getHasUsedFaucet(user1.address)
-    expect(notUsedFaucet).not.ok
+  it('drips the set amount', async () => {
+    await fweb3Faucet.setDripAmount(2)
     await fweb3Faucet.dripERC20(user1.address)
-    const hasUsedFaucet = await fweb3Faucet.getHasUsedFaucet(user1.address)
-    expect(hasUsedFaucet).ok
+    const balance = await fweb3Token.balanceOf(user1.address)
+    expect(balance).to.equal(2)
   })
 })
-
-const _weiToEth = async (_in: BigNumber): Promise<string> => {
-  const eth = await ethers.utils.formatEther(_in)
-  return eth.toString()
-}
