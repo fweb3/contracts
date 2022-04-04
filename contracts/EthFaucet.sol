489ac04eca49890e0f0a54b8253a1b8583413840
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-04-04 14:33:23 -0700

    optimize eth faucet

--- contracts/EthFaucet.sol
+++ contracts/EthFaucet.sol
@@ -9,25 +9,24 @@ import 'hardhat/console.sol';
 contract EthFaucet is Ownable {
     IERC20 private erc20RequiredToken;
     uint256 private erc20RequiredAmountToUseFaucet;
-    uint256 private ethDripAmount;
+    uint256 private dripAmount;
     uint256 private timeout;
     bool public faucetDisabled;
     bool private singleUse;
 
     mapping(address => bool) private hasUsedFaucet;
     mapping(address => uint256) private timeouts;
-    mapping(address => bool) private blockedAccounts;
 
     event ReceivedEth(address, uint256);
 
     constructor(
-        uint256 _ethDripAmount,
-        uint256 _timeout,
+        uint256 _dripAmount,
+        uint32 _timeout,
         bool _singleUse,
         IERC20 _erc20RequiredToken,
-        uint256 _erc20RequiredAmountToUseFaucet
+        uint32 _erc20RequiredAmountToUseFaucet
     ) {
-        ethDripAmount = _ethDripAmount;
+        dripAmount = _dripAmount * 10 ** 18;
         timeout = _timeout;
         singleUse = _singleUse;
         erc20RequiredToken = _erc20RequiredToken;
@@ -39,118 +38,37 @@ contract EthFaucet is Ownable {
     }
 
     function dripEth(address payable _to) external {
-        _canUseFaucet(_to);
-        (bool success, ) = _to.call{value: ethDripAmount}('');
-        require(success, 'send failed');
-        setAccountTimeout(_to, timeout);
-        setHasUsedFaucet(_to, true);
-    }
-
-    function _canUseFaucet(address _to) private view {
-        _isfaucetDisabled();
-        _isBlockedAccount(_to);
-        _checkSingleUse(_to);
-        _hasMinERC20Tokens(_to);
-        _isTimedOut(_to);
-        _contractHasEnoughEth();
-    }
-
-    function _isfaucetDisabled() private view {
         require(!faucetDisabled, 'disabled');
-    }
-
-    function _isBlockedAccount(address _account) private view {
-        require(!blockedAccounts[_account], 'blocked');
-    }
-
-    function _hasMinERC20Tokens(address _account) private view {
+        require(address(this).balance >= dripAmount, 'insufficient funds');
         require(
-            erc20RequiredToken.balanceOf(_account) >= erc20RequiredAmountToUseFaucet,
+            erc20RequiredToken.balanceOf(_to) >= erc20RequiredAmountToUseFaucet,
             'missing erc20'
         );
-    }
-
-    function _contractHasEnoughEth() private view {
-        require(address(this).balance >= ethDripAmount, 'insufficient funds');
-    }
-
-    function _isTimedOut(address _account) private view {
-        require(getAccountTimeout(_account) <= block.timestamp, 'to early');
-    }
-
-    function _checkSingleUse(address _account) private view {
         if (singleUse) {
-            require(!getHasUsedFaucet(_account), 'already used');
+            require(!hasUsedFaucet[_to], 'already used');
         }
-    }
+        require(timeouts[_to] <= block.timestamp, 'too soon');
 
-    function getDisabled() external view onlyOwner returns (bool) {
-        return faucetDisabled;
+        (bool success, ) = _to.call{value: dripAmount}('');
+
+        require(success, 'send failed');
+        timeouts[_to] = block.timestamp + timeout;
+        hasUsedFaucet[_to] = true;
     }
 
     function setDisabled(bool _val) external onlyOwner {
         faucetDisabled = _val;
     }
 
-    function setBlockedAccount(address _account, bool _val) external onlyOwner {
-        blockedAccounts[_account] = _val;
-    }
-
-    function getBlockedAccount(address _account) external view onlyOwner returns (bool) {
-        return blockedAccounts[_account];
-    }
-
-    function getBalance() external view returns (uint256) {
-        return address(this).balance;
-    }
-
-    function getDripAmount() external view onlyOwner returns (uint256) {
-        return ethDripAmount;
-    }
-
-    function setDripAmount(uint256 _amount) external onlyOwner {
-        ethDripAmount = _amount;
-    }
-
-    function getSingleUse() external view onlyOwner returns (bool) {
-        return singleUse;
-    }
-
     function setSingleUse(bool _shouldBeSingleUse) external onlyOwner {
         singleUse = _shouldBeSingleUse;
-    }
-
-    function getTimeout() external view returns (uint256) {
-        return timeout;
     }
 
     function setTimeout(uint256 _timeout) external onlyOwner {
         timeout = _timeout;
     }
 
-    function getAccountTimeout(address _account)
-        internal
-        view
-        onlyOwner
-        returns (uint256)
-    {
-        return timeouts[_account];
-    }
-
-    function setAccountTimeout(address _account, uint256 _val) internal onlyOwner {
-      timeouts[_account] = block.timestamp + _val;
-    }
-
-    function getHasUsedFaucet(address _account)
-        internal
-        view
-        onlyOwner
-        returns (bool)
-    {
-        return hasUsedFaucet[_account];
-    }
-
-    function setHasUsedFaucet(address _account, bool _val) internal onlyOwner {
-        hasUsedFaucet[_account] = _val;
+    function setDripAmount(uint256 _amount) external onlyOwner {
+        dripAmount = _amount * 10 ** 18;
     }
 }
--- test/fweb3_eth_faucet.spec.ts
+++ test/fweb3_eth_faucet.spec.ts
@@ -17,25 +17,27 @@ beforeEach(async () => {
   await fweb3Token.deployed()
 
   const FaucetFactory = await ethers.getContractFactory('EthFaucet')
-  fweb3Faucet = await FaucetFactory.deploy(1, 1, false, fweb3Token.address, 100)
+  fweb3Faucet = await FaucetFactory.deploy(100, 1, false, fweb3Token.address, 100)
   await fweb3Faucet.deployed()
 })
 
-describe('Eth faucet', async () => {
-  it('should get / set eth balance', async () => {
+describe('Eth faucet', () => {
+  it('should drip eth', async () => {
     await owner.sendTransaction({
       to: fweb3Faucet.address,
       value: ethers.utils.parseEther('666'),
     })
-    const balance = await fweb3Faucet.getBalance()
-    expect(await _weiToEth(balance)).to.equal('666.0')
+    await fweb3Token.transfer(user1.address, ethers.utils.parseEther('200'))
+    await fweb3Faucet.dripEth(user1.address)
+    const balance = await user1.getBalance()
+    expect(balance.toString()).to.equal('10100000000000000000000')
   })
 
   it('should only let the owner set eth drip amount', async () => {
     let error: any
     try {
-      const user1Faucet = await fweb3Faucet.connect(user1)
-      await user1Faucet.setDripAmount(ethers.utils.parseEther('666'))
+      const user1Faucet: EthFaucet = await fweb3Faucet.connect(user1)
+      await user1Faucet.setDripAmount(666)
     } catch (e) {
       error = e
     }
@@ -44,13 +46,17 @@ describe('Eth faucet', async () => {
 
   it('should error if drip exceeds balance', async () => {
     let error: any
+    await owner.sendTransaction({
+      to: fweb3Faucet.address,
+      value: ethers.utils.parseEther('665'),
+    })
     try {
-      await fweb3Faucet.setDripAmount(ethers.utils.parseEther('66666666'))
+      await fweb3Faucet.setDripAmount(666)
       await fweb3Faucet.dripEth(owner.address)
     } catch (e) {
       error = e
     }
-    expect(error?.message.includes('insufficient funds')).be.true
+    expect(error?.message.includes('insufficient funds')).ok
   })
 
   it('should need 200 erc20 to drip eth', async () => {
@@ -61,18 +67,6 @@ describe('Eth faucet', async () => {
       error = e
     }
     expect(error?.message.includes('missing erc20')).be.true
-  })
-
-  it('should drip eth', async () => {
-    await owner.sendTransaction({
-      to: fweb3Faucet.address,
-      value: ethers.utils.parseEther('666'),
-    })
-    await fweb3Token.transfer(user1.address, ethers.utils.parseEther('200'))
-    await fweb3Faucet.dripEth(user1.address)
-    const user1Balance = await user1.getBalance()
-    const user1Wei = await _weiToEth(user1Balance)
-    expect(user1Wei >= '10000.9').ok
   })
 
   it('only drips once when single use enabled', async () => {
@@ -92,31 +86,6 @@ describe('Eth faucet', async () => {
     expect(error?.message.includes('already used')).ok
   })
 
-  it('only owner can sign drip for eth', async () => {
-    let error: any
-    try {
-      const user1Faucet = await fweb3Faucet.connect(user1.address)
-      await user1Faucet.dripEth(user1.address)
-    } catch (e) {
-      error = e
-    }
-    expect(error?.message.includes('VoidSigner cannot sign transactions')).ok
-  })
-
-  it('blocks an account from getting drip', async () => {
-    let error: any
-    try {
-      await fweb3Faucet.setBlockedAccount(user1.address, true)
-      await fweb3Faucet.dripEth(user1.address)
-    } catch (e) {
-      error = e
-    }
-    await fweb3Faucet.setBlockedAccount(user1.address, false)
-    const isBlocked = await fweb3Faucet.getBlockedAccount(user1.address)
-    expect(isBlocked).not.ok
-    expect(error?.message.includes('blocked')).ok
-  })
-
   it('should not drip when disabled', async () => {
     let error: any
     try {
@@ -125,31 +94,24 @@ describe('Eth faucet', async () => {
     } catch (e) {
       error = e
     }
-    const isDisabled = await fweb3Faucet.getDisabled()
-    expect(isDisabled).ok
     expect(error?.message.includes('disabled')).ok
   })
-  it('gets and sets field vars', async () => {
-    const before = await fweb3Faucet.getTimeout()
-    expect(before).to.equal(1)
-    await fweb3Faucet.setTimeout(10)
-    const after = await fweb3Faucet.getTimeout()
-    expect(after).to.equal(10)
 
-    const singleUse = await fweb3Faucet.getSingleUse()
-    expect(singleUse).not.ok
-
-    const dripAmount = await fweb3Faucet.getDripAmount()
-    expect(dripAmount).to.equal(1)
-
-    await fweb3Faucet.setDripAmount(666)
-    const dripAmount2 = await fweb3Faucet.getDripAmount()
-    expect(dripAmount2).to.equal(666)
+  it('should not drip for timeout', async () => {
+    let error: any
+    try {
+      await owner.sendTransaction({
+        to: fweb3Faucet.address,
+        value: ethers.utils.parseEther('666'),
+      })
+      await fweb3Token.transfer(user1.address, ethers.utils.parseEther('200'))
+      await fweb3Faucet.setTimeout(10)
+      await fweb3Faucet.dripEth(user1.address)
+      await fweb3Faucet.dripEth(user1.address)
+    } catch (e) {
+      error = e
+      console.log(error.message)
+    }
+    expect(error?.message.includes('too soon')).ok
   })
 })
-
-const _weiToEth = async (_in: BigNumber): Promise<string> => {
-  const eth = await ethers.utils.formatEther(_in)
-  return eth.toString()
-}
-
