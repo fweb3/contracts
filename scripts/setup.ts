a04b6e6ec3701af5cd074cb5627b21a75d97a455
archae0pteryx <aarchaeopteryxx@gmail.com>
2022-05-08 11:17:26 -0700

    update deploy scripts

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,23 +1,41 @@
 import { ethers } from 'ethers'
+import hre from 'hardhat'
 import dotenv from 'dotenv'
 import token from '../artifacts/contracts/Fweb3Token.sol/Fweb3Token.json'
-// import adminNft from '../artifacts/contracts/Fweb3AdminNFT.sol/Fweb3AdminNFT.json'
-
 import fs from 'fs-extra'
 
 dotenv.config()
 
-const {
-  LOCAL_OWNER_PRIVK,
-  LOCAL_USER1_PUBKEY,
-  MUMBAI_ACCOUNT_PRIVK,
-  MUMBAI_ALCHEMY_API_KEY,
-} = process.env
+const { LOCAL_OWNER_PRIVK, MUMBAI_ACCOUNT_PRIVK, MUMBAI_ALCHEMY_API_KEY } =
+  process.env
 
-const ROOT_ADDRESS_DIR = 'deploy_addresses/mumbai'
+const _getProvider = (network: string) => {
+  if (network !== 'localhost') {
+    return new ethers.providers.AlchemyProvider(
+      'maticmum',
+      MUMBAI_ALCHEMY_API_KEY
+    )
+  } else {
+    return new ethers.providers.JsonRpcProvider()
+  }
+}
+
+type Provider =
+  | ethers.providers.JsonRpcProvider
+  | ethers.providers.AlchemyProvider
+
+const _getWallet = (network: string, provider: Provider) => {
+  if (network !== 'localhost') {
+    return new ethers.Wallet(MUMBAI_ACCOUNT_PRIVK || '', provider)
+  } else {
+    return new ethers.Wallet(LOCAL_OWNER_PRIVK || '', provider)
+  }
+}
 
 ;(async () => {
   try {
+    const { name: network } = hre.network
+    const ROOT_ADDRESS_DIR = `deploy_addresses/${network}`
     const fweb3TokenAddress = fs.readFileSync(
       `${ROOT_ADDRESS_DIR}/fweb3_token`,
       'utf-8'
@@ -30,74 +48,55 @@ const ROOT_ADDRESS_DIR = 'deploy_addresses/mumbai'
       `${ROOT_ADDRESS_DIR}/fweb3_token_faucet`,
       'utf-8'
     )
-    const provider = new ethers.providers.AlchemyProvider('maticmum', MUMBAI_ALCHEMY_API_KEY)
-    const ownerWallet = new ethers.Wallet(MUMBAI_ACCOUNT_PRIVK || '', provider)
-    // const ownerWallet = new ethers.Wallet(LOCAL_OWNER_PRIVK || '', provider)
-    // const provider = new ethers.providers.JsonRpcProvider(
-    //   'http://localhost:8545'
-    // )
+    const provider = _getProvider(network)
+    const ownerWallet = _getWallet(network, provider)
+
     const fweb3TokenContract = new ethers.Contract(
       fweb3TokenAddress,
       token.abi,
       ownerWallet
     )
-    const fweb3TokenStartBalance = await fweb3TokenContract.balanceOf(
-      ownerWallet.address
-    )
-    const userTokenStartBalance = await fweb3TokenContract.balanceOf(
-      LOCAL_USER1_PUBKEY
-    )
-    const ownerStartBalance = await provider.getBalance(ownerWallet.address)
-    const maticStartbalance = await provider.getBalance(maticFaucetAddress)
-    console.log({
-      fweb3_token_start_balance: fweb3TokenStartBalance.toString(),
-      owner_matic_start_balance: ownerStartBalance.toString(),
-      user1_token_start_balance: userTokenStartBalance.toString(),
-      matic_faucet_start_balance: maticStartbalance.toString(),
-    })
-    console.log('sending matic to matic faucet')
+
+    console.log(`[+] sending matic to matic faucet on ${network}`)
     const sendMaticToMaticFaucetTX = await ownerWallet.sendTransaction({
       to: maticFaucetAddress,
       value: ethers.utils.parseEther('1'),
     })
     await sendMaticToMaticFaucetTX.wait()
 
-    console.log('sending matic to fweb3 faucet')
+    console.log(`[+] sending matic to fweb3 faucet on ${network}`)
     const sendMaticToFweb3FaucetTX = await ownerWallet.sendTransaction({
       to: fweb3TokenFaucetAddress,
       value: ethers.utils.parseEther('1'),
     })
     await sendMaticToFweb3FaucetTX.wait()
 
-    console.log('sending fweb3 to faucet')
+    console.log(`[+] sending fweb3 to faucet on ${network}`)
     const fweb3ToFaucetTX = await fweb3TokenContract.transfer(
       fweb3TokenFaucetAddress,
       ethers.utils.parseEther('69420')
     )
     await fweb3ToFaucetTX.wait()
 
-    // console.log('sending fweb3 to user1')
-    // const fweb3TransferTX = await fweb3TokenContract.transfer(
-    //   LOCAL_USER1_PUBKEY,
-    //   ethers.utils.parseEther('300')
-    // )
-    // await fweb3TransferTX.wait()
+    const fweb3_faucet_token_balance = await fweb3TokenContract.balanceOf(
+      fweb3TokenFaucetAddress
+    )
+    const fweb3_faucet_matic_balance = await provider.getBalance(
+      fweb3TokenFaucetAddress
+    )
+    const matic_faucet_balance = await provider.getBalance(maticFaucetAddress)
 
-    // const fweb3TokenEndBalance = await fweb3TokenContract.balanceOf(
-    //   ownerWallet.address
-    // )
-    // const userTokenEndBalance = await fweb3TokenContract.balanceOf(
-    //   LOCAL_USER1_PUBKEY
-    // )
-    // const ownerEndBalance = await provider.getBalance(ownerWallet.address)
-    // const maticEndbalance = await provider.getBalance(maticFaucetAddress)
-
-    // console.log({
-    //   fweb3_token_end_balance: fweb3TokenEndBalance.toString(),
-    //   owner_matic_end_balance: ownerEndBalance.toString(),
-    //   user1_token_end_balance: userTokenEndBalance.toString(),
-    //   matic_faucet_end_balance: maticEndbalance.toString(),
-    // })
+    console.log({
+      fweb3_faucet_token_balance: ethers.utils.formatEther(
+        fweb3_faucet_token_balance.toString()
+      ),
+      fweb3_faucet_matic_balance: ethers.utils.formatEther(
+        fweb3_faucet_matic_balance.toString()
+      ),
+      matic_faucet_balance: ethers.utils.formatEther(
+        matic_faucet_balance.toString()
+      ),
+    })
     console.log('done')
   } catch (e) {
     console.error(e)
--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,20 +1,21 @@
 import fs from 'fs-extra'
+import hre from 'hardhat'
 
 const writeAddressToFile = (fileName: string, address: string): void => {
-  const path = `deploy_addresses/${process.env.NETWORK}/${fileName}`
+  const path = `deploy_addresses/${hre.network.name}/${fileName}`
   fs.writeFileSync(path, address)
   console.log(`wrote ${fileName} address to ${path}`)
 }
 
 const readAddressFromFile = (fileName: string): string => {
-  const path = `deploy_addresses/${process.env.NETWORK}/${fileName}`
+  const path = `deploy_addresses/${hre.network.name}/${fileName}`
   const address = fs.readFileSync(path, 'utf-8')
   return address
 }
 
 const backupAddresses = () => {
   const backupPath = `deploy_addresses/backup/${Date.now()}`
-  fs.copySync(`deploy_addresses/${process.env.NETWORK}`, backupPath)
+  fs.copySync(`deploy_addresses/${hre.network.name}`, backupPath)
 }
 
 export { writeAddressToFile, readAddressFromFile, backupAddresses }
